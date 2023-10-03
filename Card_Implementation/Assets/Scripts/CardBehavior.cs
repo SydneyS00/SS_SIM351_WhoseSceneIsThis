be50313c327964c1c873432315a17954983e5a28
SydneyS00 <sydschmitz03@outlook.com>
2023-10-03 11:56:44 -0700

    New Build

---
+++ Card_Implementation/Assets/Scripts/CardBehavior.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBehavior : MonoBehaviour
+{
+    public bool hasBeenPlayed;
+    public int handIndex;
+
+    private GameManager gm;
+
+    private void Start()
+    {
+        gm = FindObjectOfType<GameManager>(); 
+    }
+
+    private void OnMouseDown()
+    {
+        if (hasBeenPlayed == false)
+        {
+            transform.position += Vector3.up * 5;
+            hasBeenPlayed = true;
+            gm.availableCardsSlots[handIndex] = true;
+
+            Invoke("MoveToDiscardPile", 2f);
+        }
+    }
+
+    void MoveToDiscardPile()
+    {
+        gm.discardPile.Add(this);
+        gameObject.SetActive(false);
+
+        Debug.Log("Card discarded"); 
+    }
+}
